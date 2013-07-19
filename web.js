cf7134735d685259d994d5e503aeced6fdd08c4d
Peter Gok <petergo@live.com>
2013-07-19 04:49:31 +0000

    Took out the require buf

--- web.js
+++ web.js
@@ -1,12 +1,11 @@
 var express = require('express');
 var fs = require ('fs');
-var buf = require ('buf');
 var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
   var buffer = new Buffer (fs.readFileSync("index.html"), "utf-8");
-  response.send(buffer.toString('utf-8', 0, 27));
+  response.send(buffer.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
